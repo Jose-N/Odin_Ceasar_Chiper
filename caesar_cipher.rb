c018b42a9c401d550e501e51d3bc78c8b8fec65b
Jose Naylor <jose.c.naylor@gmail.com>
2017-03-10 23:56:30 +0000

    letter can now wrap around from z back to a

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,15 @@
 #logic to preform shift
-def caesar_chiper(phrase, shift)
+def caesar_chiper(phrase, number)
     #Array containing A-Z
     alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     chiper = ''
     phrase.each_char do |i|
-       newletter = alpha[alpha.index(phrase[i])+shift]
+       shift = alpha.index(phrase[i])+number
+       if shift > alpha.length then
+           shift = shift - alpha.length 
+       end
+       newletter = alpha[shift]
        chiper.to_s <<  newletter.to_s
-        chiper
     end
     puts chiper
 end
