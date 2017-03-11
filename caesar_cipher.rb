d8fda4d29c0a407d673e014a3c38a1f8be67d7d1
Jose Naylor <jose.c.naylor@gmail.com>
2017-03-11 00:12:05 +0000

    Use of white spaces or special characters doesnt break program

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,15 +4,19 @@ def caesar_chiper(phrase, number)
     alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     chiper = ''
     phrase.each_char do |i|
-       shift = alpha.index(phrase[i])+number
-       if shift > alpha.length then
-           shift = shift - alpha.length 
+       if alpha.include?(i) then
+            shift = alpha.index(phrase[i])+number
+            if shift > alpha.length then
+                shift = shift - alpha.length 
+            end
+            newletter = alpha[shift]
+            chiper.to_s <<  newletter.to_s
+       else
+            chiper.to_s << i.to_s
        end
-       newletter = alpha[shift]
-       chiper.to_s <<  newletter.to_s
     end
     puts chiper
 end
-
+ 
 #Cipher (phrase and shift)
-caesar_chiper('what', 5)
+caesar_chiper('what a string!', 5)
